7f76d14d661eb93e24a65c1ef336476e6eea5e52
Bianca Antunes <antunesmariabianca@hotmail.com>
2023-08-09 21:47:16 -0300

    Update App.tsx

--- Aula I/fundamentos2/App.tsx
+++ Aula I/fundamentos2/App.tsx
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={{flex:1, padding:24, backgroundColor: '#111'}}>
-      <Text style={{color: '#fff', fontSize:24, fontWeight:'bold'}}>Nome do grupo</Text>
-      <Text >Quarta, 09 de agosto</Text>
+      <Text style={{color: '#fff', fontSize:24, fontWeight:'bold', marginTop: 48}}>Nome do grupo</Text>
+      <Text style={styles.texto}>Quarta, 09 de agosto de 2023</Text>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  texto : {
+    color: '#c7c7c7',
+    fontSize:16
+  }
+})
